173eb4edfe4f1429a5c8fd3b2ff29fd9b0eb75ff
koustav <koustavm.aeie2018@nsec.ac.in>
2023-05-10 07:08:18 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,54 +1,54 @@
-import { useEffect, useState } from "react"
-import { NewTodoForm } from "./components/NewTodoForm"
-import TodoList from "./components/TodoList"
+import { useEffect, useState } from "react";
+import { NewTodoForm } from "./components/NewTodoForm";
+import TodoList from "./components/TodoList";
 
-import "./styles.css"
-
+import "./styles.css";
 
 export default function App() {
   const [todos, setTodos] = useState(() => {
-    const localValue = localStorage.getItem("ITEMS")
-    if (localValue == null) return []
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
 
-    return JSON.parse(localValue)
-  })
+    return JSON.parse(localValue);
+  });
 
   useEffect(() => {
-    localStorage.setItem("ITEMS", JSON.stringify(todos))
-  }, [todos])
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(title) {
-    setTodos(currentTodos => {
+    setTodos((currentTodos) => {
       return [
         ...currentTodos,
         { id: crypto.randomUUID(), title, completed: false },
-      ]
-    })
+      ];
+    });
   }
 
   function toggleTodo(id, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
         if (todo.id === id) {
-          return { ...todo, completed }
+          return { ...todo, completed };
         }
 
-        return todo
-      })
-    })
+        return todo;
+      });
+    });
   }
 
   function deleteTodo(id) {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
-    })
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
   }
 
   return (
     <>
-      <NewTodoForm onSubmit={addTodo}/>
+      <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
       <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+      <h4>you have {todos.length} todos</h4>
     </>
-  )
+  );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,7 +3,7 @@ import { TodoItem } from "./TodoItem"
 export default function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
     <ul className="list">
-      {todos.length === 0 && "No Todos"}
+      
       {todos.map(todo => {
         return (
           <TodoItem
